675b853759370097be4b58df65bd3bd524ec6d88
luck07051 <luck0751@gmail.com>
2022-12-14 22:48:01 +0800

    config.

--- config.h
+++ config.h
@@ -1,19 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int topbar = 1;                  /* -b  option; if 0, dmenu appears at bottom */
+static int fuzzy = 1;                   /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static char *prompt = NULL;             /* -p  option; prompt to the left of input field */
+static unsigned int lines = 0;          /* -l  option; if nonzero, dmenu uses vertical list
+					 *             with given number of lines */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[] = "monospace:size=10";
+static char font[] = "MesloLGS Nerd Font:size=10";
+static char font2[] = "Noto Sans CJK TC:size=10";
 static const char *fonts[] = {
 	font,
+	font2,
 	"monospace:size=10",
 };
 
-static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-
-static const unsigned int baralpha = 0xd0;
+static const unsigned int baralpha = 0xff;
 static const unsigned int borderalpha = OPAQUE;
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -31,8 +34,6 @@ static char *colors[SchemeLast][2] = {
 	[SchemeSel]  = { selfgcolor,  selbgcolor  },
 	[SchemeOut]  = { "#000000",   "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -44,7 +45,6 @@ static const char worddelimiters[] = " ";
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-	{ "font",        STRING, &font },
 	{ "normfgcolor", STRING, &normfgcolor },
 	{ "normbgcolor", STRING, &normbgcolor },
 	{ "selfgcolor",  STRING, &selfgcolor },
